0fc5a4a3c18c359b8b8e13b94fa21fafc2e25856
JUGADORUS <savelilya07@gmail.com>
2024-05-30 20:27:49 +0500

    fix

--- Assets/Counter.cs
+++ Assets/Counter.cs
@@ -6,9 +6,32 @@ using UnityEngine;
 public class Counter : MonoBehaviour
 {
     public event Action Changed;
+    public float _currentTicks = 0;
+    private float _timeBetweenTicks = 0.5f;
+    private bool isPaused = false;
 
     private void OnMouseDown()
     {
-        Changed?.Invoke();
+        ChangeActiveStatus();
+    }
+
+    private void ChangeActiveStatus()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused == false)
+        {
+            StartCoroutine(ChangeTime());
+        }
+    }
+
+    private IEnumerator ChangeTime()
+    {
+        while (isPaused == false)
+        {
+            yield return new WaitForSeconds(_timeBetweenTicks);
+            _currentTicks++;
+            Changed?.Invoke();
+        }
     }
 }
--- Assets/CounterView.cs
+++ Assets/CounterView.cs
@@ -1,44 +1,22 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CounterView : MonoBehaviour
 {
     [SerializeField] private Counter _counter;
 
-    private float _timeBetweenTicks = 0.5f;
-    private float _currentTicks = 0;
-    private bool isPaused = false;
-
     private void OnEnable()
     {
-        _counter.Changed += ChangeActiveStatus;
+        _counter.Changed += ShowCount;
     }
 
     private void OnDisable()
     {
-        _counter.Changed -= ChangeActiveStatus;
+        _counter.Changed -= ShowCount;
     }
 
-    private void ChangeActiveStatus()
+    private void ShowCount()
     {
-        isPaused = !isPaused;
-
-        if (isPaused == false)
-        {
-            StartCoroutine(ChangeTime());
-        }
+        Debug.Log(_counter._currentTicks);
     }
-
-    private IEnumerator ChangeTime()
-    {
-        while (isPaused == false)
-        {
-            yield return new WaitForSeconds(_timeBetweenTicks);
-            _currentTicks++;
-            Debug.Log(_currentTicks);
-            yield return null;
-        }
-    }
-
 }
